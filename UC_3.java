322fe1abfa2e1fd3f31eebb69e84a8ddfb05b550
shreyyyyyy112 <sm0262@srmist.edu.in>
2026-02-22 22:30:02 +0530

    UC3

---
+++ UC_3.java
@@ -0,0 +1,14 @@
+public class UC_3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ","   ***   ",  "   ***   ",  " ****** ",  "  ***** "));
+        System.out.println(String.join(" ","  ** **  ",  "  ** **  ",  " **   **",  " **     "));
+        System.out.println(String.join(" "," **   ** ",  " **   ** ",  " **   **",  " **     "));
+        System.out.println(String.join(" "," **   ** ",  " **   ** ",  " ****** ",  "   **** "));
+        System.out.println(String.join(" "," **   ** ",  " **   ** ",  " **     ",  "        "));
+        System.out.println(String.join(" ","  ** **  ",  "  ** **  ",  " **     ",  " **    "));
+        System.out.println(String.join(" ","   ***   ",  "   ***   ",  " **     ",  "  *****"));
+
+    }
+}
+
